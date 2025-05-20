6691bc4cc99f68ffb4446c9bc92fe72a05af9138
Al Amin <linkers4me@gmail.com>
2025-05-21 01:15:44 +0600

    added home page content section-2

--- components/footer.php
+++ components/footer.php
@@ -1,4 +1,15 @@
+        <footer class="relative mt-20 bg-slate-900 px-4 pt-20">
+            <div class="absolute -top-10 left-1/2 h-16 w-16 -translate-x-1/2 rounded-full bg-white p-2"><img class="w-full object-cover" src="/images/profile.jpeg" alt="" /></div>
+            <nav aria-label="Footer Navigation" class="mx-auto mb-10 flex max-w-lg flex-col gap-10 text-center sm:flex-row sm:text-left">
+                <a href="#" class="font-medium text-white">Demo</a>
+                <a href="#" class="font-medium text-white">Support</a>
+                <a href="#" class="font-medium text-white">Privacy Policy</a>
+                <a href="#" class="font-medium text-white">Terms & Conditions</a>
+            </nav>
+            <p class="py-10 text-center text-gray-300">© 2025 Al Amin | All Rights Reserved</p>
+        </footer>
 
-    <script src="main.js"></script>
-</body>
+        <script src="main.js"></script>
+    </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,12 +1,32 @@
 <?php include 'components/header.php' ?>
-    <main class="min-h-[90vh]">
-        <section class="flex flex-col md:flex-row gap-4">
-            <div class="intro max-w-[60%]">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, dolores eos voluptatem temporibus eum soluta eaque ratione quo, laborum ullam in accusamus provident, placeat repudiandae quidem dolorem veritatis fugiat. Debitis?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus voluptate quidem provident eius praesentium ut, quod minima maiores mollitia! Optio eius sit ducimus saepe ipsum iusto minus ratione, tenetur dolorum.
+<main class="min-h-[90vh]">
+    <section class="hero section min-h-[70vh] flex flex-col md:flex-row gap-4 lg:gap-12 mx-auto items-center justify-center my-12">
+        <div class="intro max-w-[60%] lg:max-w-[800px]">
+            <h1 class="mb-8 font-bold font-mono text-4xl"><span class="underline underline-offset-4 decoration-4">Hi!</span> I'm Al Amin</h1>
+            <div class="text-lg/8 font-mono text-slate-200 text-justify">
+                <p>This is some individual introduction. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, dolores eos voluptatem temporibus eum soluta eaque ratione quo, laborum ullam in accusamus provident, placeat repudiandae quidem dolorem veritatis fugiat. Debitis?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus voluptate quidem provident eius praesentium ut, quod minima maiores mollitia! Optio eius sit ducimus saepe ipsum iusto minus ratione, tenetur dolorum.</p>
             </div>
-            <div class="intro-image overflow-hidden w-[40%] max-w-[320px] rounded-full">
-                <img src="/images/profile.jpg" alt="Profile photo" class="w-full">
+        </div>
+        <div class="intro-image overflow-hidden w-[40%] max-w-[320px] rounded-full">
+            <img src="/images/profile.jpeg" alt="Profile photo" class="w-full">
+        </div>
+    </section>
+
+    <section class="carrier-mission my-12">
+        <div class="container mx-auto">
+            <h1 class="mb-8 font-bold font-mono text-3xl text-center underline underline-offset-8">Carrier Mission & Vision</h1>
+            <div class="flex flex-col md:flex-row gap-4 md:gap-8 my-12">
+                <div class="carrier-vision-img w-[40%]">
+                    <img src="/images/home-section-2.jpg" alt="Carrier Mission & Vision">
+                </div>
+                <div class="carrier-text text-justify leading-8 w-[60%]">
+                    <p>With the objective Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum beatae, repellendus, officiis dolor eius nisi excepturi, natus dolore accusamus officia amet qui modi perspiciatis optio libero autem voluptate incidunt esse? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias ex nesciunt non nulla reprehenderit fugit consequuntur, quod incidunt est, nisi explicabo! Eligendi natus quia blanditiis, a provident unde voluptatem dicta. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas ut veniam quisquam velit, neque autem tenetur voluptates maiores cum, tempore expedita odio incidunt libero! Tempora quaerat vel voluptatibus voluptatum reprehenderit.</p>
+                    <p>Vision to Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam labore ad placeat dolore voluptatum optio? Quas voluptas rem, ad ea excepturi omnis iste suscipit porro nam praesentium eum incidunt sit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, adipisci cupiditate. Eveniet, minus laudantium numquam laboriosam aspernatur doloribus illo ratione repellat nam distinctio laborum ullam voluptatibus modi delectus tempore fuga.</p>
+                </div>
             </div>
-        </section>
-    </main>
+
+        </div>
+
+    </section>
+</main>
 <?php include 'components/footer.php' ?>
