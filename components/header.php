cc45b338d4080ada4b4d08ba5c3dcd3258b16992
al-amin404 <linkers4me@gmail.com>
2025-05-20 22:37:17 +0600

    initial commit

---
+++ components/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal Portfolio</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body class="bg-slate-800 text-white">
+    <header class="h-[80px]">
+        <div class="flex items-center justify-between mx-auto py-4 px-8">
+            <div class="h-12">
+                <img src="/images/logo.jpg" alt="My Portfolio" class="w-full font-bold font-serif text-xl">
+            </div>
+            <nav class="transition-all duration-500">
+                <input type="checkbox" name="" id="toggle-sidebar">
+                <label for="toggle-sidebar" class="open-sidebar-btn"><i class="fa-solid fa-bars text-xl"></i></label>
+                <ul
+                    class="nav-link-container flex flex-col md:flex-row gap-8 decoration-none *:font-semibold *:hover:underline *:underline-offset-8 *:text-slate-400 *:hover:text-white">
+                    <label for="toggle-sidebar" class="close-sidebar-btn text-white text-base"><i class="fa-solid fa-xmark text-base"></i> Close</label>
+                    <a href="/">
+                        <li class="text-white underline">Home</li>
+                    </a>
+                    <a href="">
+                        <li>About</li>
+                    </a>
+                    <a href="">
+                        <li>Portfolio</li>
+                    </a>
+                    <a href="">
+                        <li>Services</li>
+                    </a>
+                    <a href="">
+                        <li>Contact</li>
+                    </a>
+                </ul>
+            </nav>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include 'components/header.php' ?>
+    <main class="min-h-[90vh]">
+        <section class="flex flex-col md:flex-row gap-4">
+            <div class="intro max-w-[60%]">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, dolores eos voluptatem temporibus eum soluta eaque ratione quo, laborum ullam in accusamus provident, placeat repudiandae quidem dolorem veritatis fugiat. Debitis?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus voluptate quidem provident eius praesentium ut, quod minima maiores mollitia! Optio eius sit ducimus saepe ipsum iusto minus ratione, tenetur dolorum.
+            </div>
+            <div class="intro-image overflow-hidden w-[40%] max-w-[320px] rounded-full">
+                <img src="/images/profile.jpg" alt="Profile photo" class="w-full">
+            </div>
+        </section>
+    </main>
+<?php include 'components/footer.php' ?>
